3fa308a70b68436172c8090ee16adfa810848558
Karan Anchan <kar.anchan02@gmail.com>
2024-05-27 14:23:03 +0530

    Added plot_selections Function

--- visualization.py
+++ visualization.py
@@ -18,3 +18,20 @@ def plot_rewards(rewards):
     plt.grid(True)
     plt.savefig('10_arm_testbed_performance.png')
     plt.show()
+    
+def plot_selections(selections):
+    
+    """
+    Plot the number of times each arm was selected.
+    
+    Parameters:
+        selections (ndarray): Array containing the count of selections for each arm.
+    """
+    
+    plt.figure(figsize=(10, 5))
+    plt.bar(range(len(selections)), selections, color='green')
+    plt.xlabel('Arms')
+    plt.ylabel('Number of times selected')
+    plt.title('Selections of Each Arm')
+    plt.savefig('10_arm_testbed_selections.png')
+    plt.show()
